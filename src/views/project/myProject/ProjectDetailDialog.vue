050fc643e9f0599ce089d06c2bc8333cf4b5c3c8
zheng <zhenghuiguo@zohomail.cn>
2024-07-18 15:30:40 +0800

    fix:添加了我的项目和专利的两个详情弹窗

---
+++ src/views/project/myProject/ProjectDetailDialog.vue
@@ -0,0 +1,79 @@
+<template>
+  <el-dialog :model-value="visible" @update:model-value="updateVisible" width="50%">
+    <div class="sticky-container">
+      <el-tabs>
+        <el-tab-pane label="基本信息" name="first">
+          <div style="margin-top: 10px"></div>
+          <el-descriptions-item label="基本信息" :span="2"></el-descriptions-item>
+          <el-descriptions class="margin-top" title="" :column="2" :label-style="{ width: '25%' }" :content-style="{ width: '25%' }" border>
+            <el-descriptions-item label="项目名称">{{}} </el-descriptions-item>
+            <el-descriptions-item label="项目任务书编号">{{}} </el-descriptions-item>
+            <el-descriptions-item label="负责课题">{{}} </el-descriptions-item>
+            <el-descriptions-item label="课题任务书编号">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目牵头单位">{{}}</el-descriptions-item>
+            <el-descriptions-item label="是否牵头单位">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目负责人">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目专员/联系人">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目级别">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目来源">{{}}</el-descriptions-item>
+            <el-descriptions-item label="立项时间">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目计划验收时间">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目执行时间（年）">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目推进情况">{{}}</el-descriptions-item>
+            <el-descriptions-item label="合作单位">{{}}</el-descriptions-item>
+            <el-descriptions-item label="涉及专家、团队">{{}}</el-descriptions-item>
+            <el-descriptions-item label="项目经费总额">{{}}</el-descriptions-item>
+            <el-descriptions-item label="专项经费">{{}}</el-descriptions-item>
+            <el-descriptions-item label="自筹经费">{{}}</el-descriptions-item>
+          </el-descriptions>
+          <el-collapse>
+            <el-collapse-item style="font-size: 20px" name="2">
+              <template #title>
+                <span style="font-size: 14px">项目简介</span>
+              </template>
+              <div class="unselectable-textbox" style="font-size: 14px; color: #606266"></div>
+            </el-collapse-item>
+          </el-collapse>
+        </el-tab-pane>
+        <el-tab-pane label="项目计划" name="second">
+          <ProjectPlan></ProjectPlan>
+        </el-tab-pane>
+        <el-tab-pane label="项目指标" name="third">
+          <ProjectIndicators></ProjectIndicators>
+        </el-tab-pane>
+        <el-tab-pane label="项目成员" name="fourth">
+          <Member />
+        </el-tab-pane>
+        <el-tab-pane label="项目经费" name="fifth">
+          <FundsDetail></FundsDetail>
+        </el-tab-pane>
+        <el-tab-pane label="项目申报附件" name="sixth">
+          <Projectfile></Projectfile>
+        </el-tab-pane>
+        <el-tab-pane label="项目推进情况" name="seventh">
+          <ProjectProgress></ProjectProgress>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps<{ visible: boolean }>();
+const emits = defineEmits(['update:visible']);
+
+const updateVisible = (value: boolean) => {
+  emits('update:visible', value);
+};
+
+const closeDialog = () => {
+  emits('update:visible', false);
+};
+
+const confirmDialog = () => {
+  // 处理确定逻辑
+  emits('update:visible', false);
+};
+</script>
---
+++ src/views/research/IntellectualProperty/ipDetailDialog.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-dialog :model-value="visible" title="详情" width="50%" @update:model-value="updateVisible">
+    <el-collapse>
+      <el-collapse-item name="1">
+        <template #title>
+          <span>知识产权成员列表</span>
+        </template>
+        <el-table ref="multipleTable" border style="width: 100%" :row-style="{ height: '50px' }" :cell-style="{ padding: '0px' }">
+          <el-table-column label="姓名" :resizable="false" align="center" prop="nickName"> </el-table-column>
+          <el-table-column label="职称" :resizable="false" align="center" prop="jobTitle"> </el-table-column>
+          <el-table-column label="学历" :resizable="false" align="center" prop="diploma"> </el-table-column>
+          <el-table-column label="邮箱" :resizable="false" align="center" prop="email"> </el-table-column>
+          <el-table-column label="手机号码" :resizable="false" align="center" prop="phonenumber"> </el-table-column>
+          <el-table-column label="部门名称" :resizable="false" align="center" prop="deptName"> </el-table-column>
+        </el-table>
+      </el-collapse-item>
+      <el-collapse-item name="2">
+        <template #title>
+          <span>知识产权附件列表</span>
+        </template>
+        <el-table ref="multipleTable" border style="width: 100%" :row-style="{ height: '50px' }" :cell-style="{ padding: '0px' }">
+          <el-table-column label="文件名称" :resizable="false" align="center" :show-overflow-tooltip="true" width="500">
+            <template> {{}} </template>
+          </el-table-column>
+          <el-table-column :label="'文件格式'" align="center" prop="fileSuffix" :show-overflow-tooltip="true" width="120">
+            <template #default="scope">
+              <div width="120">
+                <el-tag v-if="scope.row.fileSuffix === '.pdf'" type="success" size="small">{{ scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.docx'" type="warning" size="small">{{ scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.doc'" type="warning" size="small">{{ scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.xls'" size="small">{{ scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.xlsx'" size="small">{{ scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.ppt'" type="danger" size="small">{{ scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.pptx'" type="danger" size="small">{{ scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else type="info" size="small">{{ scope.row.fileSuffix }}</el-tag>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="上传人" :resizable="false" align="center" prop="createBy"> </el-table-column>
+          <el-table-column :label="'操作'" :resizable="false" align="center" min-width="100px">
+            <template> </template>
+          </el-table-column>
+        </el-table>
+      </el-collapse-item>
+    </el-collapse>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps<{ visible: boolean }>();
+const emits = defineEmits(['update:visible']);
+
+const updateVisible = (value: boolean) => {
+  emits('update:visible', value);
+};
+
+const closeDialog = () => {
+  emits('update:visible', false);
+};
+
+const confirmDialog = () => {
+  // 处理确定逻辑
+  emits('update:visible', false);
+};
+// 使用 watch 监听 updateVisible 变化并打印到控制台
+watch(
+  () => props.visible,
+  (newValue, oldValue) => {
+    console.log('Visible changed:', newValue);
+  }
+);
+</script>
+
+<style scoped>
+/* 你的样式 */
+</style>
