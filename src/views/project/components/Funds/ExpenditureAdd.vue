5f429a12549b681f3990a4b4b7ddc85f0bbcd527
范佳兴 <c1111111c@163.com>
2024-07-24 16:54:48 +0800

    feat:新增项目大事记详情对话框组件

--- src/views/project/components/Funds/ExpenditureAdd.vue
+++ src/views/project/components/Funds/ExpenditureAdd.vue
@@ -131,10 +131,7 @@ const form = ref({
   thirdLevelSubject: ''
 });
 
-const zxzcOptions = ref([
-  // { zxzcId: '0', zxzcName: '专项' },
-  // { zxzcId: '1', zxzcName: '自筹' }
-]);
+const zxzcOptions = ref([]);
 
 const getZxzcOptions = () => {
   getDicts('pro_zxzc_options').then((resp) => {
@@ -147,10 +144,7 @@ const getZxzcOptions = () => {
   });
 };
 
-const zjjjOptions = ref([
-  // { zjjjId: '0', zjjjName: '直接' },
-  // { zjjjId: '1', zjjjName: '间接' }
-]);
+const zjjjOptions = ref([]);
 
 const getZjjjOptions = () => {
   getDicts('pro_zjjj_options').then((resp) => {
@@ -163,31 +157,7 @@ const getZjjjOptions = () => {
   });
 };
 
-const firstLevelSubjectOptions = ref([
-  // { firstLevelSubjectId: '0', firstLevelSubjectName: '设备费' },
-  // { firstLevelSubjectId: '1', firstLevelSubjectName: '业务费' },
-  // { firstLevelSubjectId: '2', firstLevelSubjectName: '劳务费' },
-  // { firstLevelSubjectId: '3', firstLevelSubjectName: '材料费' },
-  // { firstLevelSubjectId: '4', firstLevelSubjectName: '科研活动费' },
-  // { firstLevelSubjectId: '5', firstLevelSubjectName: '科研服务费' },
-  // { firstLevelSubjectId: '6', firstLevelSubjectName: '人员和劳务补助费' },
-  // { firstLevelSubjectId: '7', firstLevelSubjectName: '绩效支出' },
-  // { firstLevelSubjectId: '8', firstLevelSubjectName: '管理费' },
-  // { firstLevelSubjectId: '9', firstLevelSubjectName: '房屋租赁费' },
-  // { firstLevelSubjectId: '10', firstLevelSubjectName: '日常水电暖费' },
-  // { firstLevelSubjectId: '11', firstLevelSubjectName: '差旅费' },
-  // { firstLevelSubjectId: '12', firstLevelSubjectName: '数据样本采集费' },
-  // { firstLevelSubjectId: '13', firstLevelSubjectName: '测试化验加工费' },
-  // { firstLevelSubjectId: '14', firstLevelSubjectName: '燃料动力费' },
-  // { firstLevelSubjectId: '15', firstLevelSubjectName: '办公费' },
-  // { firstLevelSubjectId: '16', firstLevelSubjectName: '印刷/出版费' },
-  // { firstLevelSubjectId: '17', firstLevelSubjectName: '知识产权事务费' },
-  // { firstLevelSubjectId: '18', firstLevelSubjectName: '车辆使用费' },
-  // { firstLevelSubjectId: '19', firstLevelSubjectName: '差旅费' },
-  // { firstLevelSubjectId: '20', firstLevelSubjectName: '会议/会务费' },
-  // { firstLevelSubjectId: '21', firstLevelSubjectName: '专家咨询费' },
-  // { firstLevelSubjectId: '22', firstLevelSubjectName: '其他费用' }
-]);
+const firstLevelSubjectOptions = ref([]);
 
 const getFirstLevelSubjectOptions = () => {
   getDicts('pro_first_subject').then((resp) => {
@@ -200,30 +170,7 @@ const getFirstLevelSubjectOptions = () => {
   });
 };
 
-const secondLevelSubjectOptions = ref([
-  // { secondLevelSubjectId: '0', secondLevelSubjectName: '购置设备费' },
-  // { secondLevelSubjectId: '1', secondLevelSubjectName: '试制设备费' },
-  // { secondLevelSubjectId: '2', secondLevelSubjectName: '设备升级改造费' },
-  // { secondLevelSubjectId: '3', secondLevelSubjectName: '设备租赁费' },
-  // { secondLevelSubjectId: '4', secondLevelSubjectName: '材料费' },
-  // { secondLevelSubjectId: '5', secondLevelSubjectName: '资料费' },
-  // { secondLevelSubjectId: '6', secondLevelSubjectName: '数据/样本采集费' },
-  // { secondLevelSubjectId: '7', secondLevelSubjectName: '测试化验加工费' },
-  // { secondLevelSubjectId: '8', secondLevelSubjectName: '燃料动力费' },
-  // { secondLevelSubjectId: '9', secondLevelSubjectName: '办公费' },
-  // { secondLevelSubjectId: '10', secondLevelSubjectName: '印刷/出版费' },
-  // { secondLevelSubjectId: '11', secondLevelSubjectName: '知识产权事务费' },
-  // { secondLevelSubjectId: '12', secondLevelSubjectName: '车辆使用费' },
-  // { secondLevelSubjectId: '13', secondLevelSubjectName: '出版/文献/信息传播/知识产权事务费' },
-  // { secondLevelSubjectId: '14', secondLevelSubjectName: '差旅费' },
-  // { secondLevelSubjectId: '15', secondLevelSubjectName: '会议/会务费' },
-  // { secondLevelSubjectId: '16', secondLevelSubjectName: '国内协作费' },
-  // { secondLevelSubjectId: '17', secondLevelSubjectName: '国际合作交流费' },
-  // { secondLevelSubjectId: '18', secondLevelSubjectName: '专家咨询费' },
-  // { secondLevelSubjectId: '19', secondLevelSubjectName: '人员劳务费' },
-  // { secondLevelSubjectId: '20', secondLevelSubjectName: '会议/差旅/国际合作与交流费' },
-  // { secondLevelSubjectId: '21', secondLevelSubjectName: '无' }
-]);
+const secondLevelSubjectOptions = ref([]);
 
 const getSecondLevelSubjectOptions = () => {
   getDicts('pro_second_subject').then((resp) => {
@@ -236,16 +183,7 @@ const getSecondLevelSubjectOptions = () => {
   });
 };
 
-const thirdLevelSubjectOptions = ref([
-  // { thirdLevelSubjectId: '0', thirdLevelSubjectName: '无' },
-  // { thirdLevelSubjectId: '1', thirdLevelSubjectName: '知识产权事务费' },
-  // { thirdLevelSubjectId: '2', thirdLevelSubjectName: '印刷打印制作费' },
-  // { thirdLevelSubjectId: '3', thirdLevelSubjectName: '文献数据库费' },
-  // { thirdLevelSubjectId: '4', thirdLevelSubjectName: '信息传播费' },
-  // { thirdLevelSubjectId: '5', thirdLevelSubjectName: '会议费' },
-  // { thirdLevelSubjectId: '6', thirdLevelSubjectName: '差旅费' },
-  // { thirdLevelSubjectId: '7', thirdLevelSubjectName: '国际合作费' }
-]);
+const thirdLevelSubjectOptions = ref([]);
 
 const getThirdLevelSubjectOptions = () => {
   getDicts('pro_third_subject').then((resp) => {
