54e6ea2b4d37ea10c6539604827476c5dcfbafc1
Pasiner <1169983244@qq.com>
2024-08-02 16:59:40 +0800

    修改了首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,165 +1,451 @@
 <template>
-  <div class="app-container home">
-    <el-row :gutter="20">
-      <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>RuoYi-Vue-Plus多租户管理系统</h2>
-        <p>
-          RuoYi-Vue-Plus 是基于 RuoYi-Vue 针对 分布式集群 场景升级(不兼容原框架)
-          <br />
-          * 前端开发框架 Vue3、TS、Element Plus<br />
-          * 后端开发框架 Spring Boot<br />
-          * 容器框架 Undertow 基于 Netty 的高性能容器<br />
-          * 权限认证框架 Sa-Token 支持多终端认证系统<br />
-          * 关系数据库 MySQL 适配 8.X 最低 5.7<br />
-          * 缓存数据库 Redis 适配 6.X 最低 4.X<br />
-          * 数据库框架 Mybatis-Plus 快速 CRUD 增加开发效率<br />
-          * 数据库框架 p6spy 更强劲的 SQL 分析<br />
-          * 多数据源框架 dynamic-datasource 支持主从与多种类数据库异构<br />
-          * 序列化框架 Jackson 统一使用 jackson 高效可靠<br />
-          * Redis客户端 Redisson 性能强劲、API丰富<br />
-          * 分布式限流 Redisson 全局、请求IP、集群ID 多种限流<br />
-          * 分布式锁 Lock4j 注解锁、工具锁 多种多样<br />
-          * 分布式幂等 Lock4j 基于分布式锁实现<br />
-          * 分布式链路追踪 SkyWalking 支持链路追踪、网格分析、度量聚合、可视化<br />
-          * 分布式任务调度 SnailJob 高性能 高可靠 易扩展<br />
-          * 文件存储 Minio 本地存储<br />
-          * 文件存储 七牛、阿里、腾讯 云存储<br />
-          * 监控框架 SpringBoot-Admin 全方位服务监控<br />
-          * 校验框架 Validation 增强接口安全性 严谨性<br />
-          * Excel框架 Alibaba EasyExcel 性能优异 扩展性强<br />
-          * 文档框架 SpringDoc、javadoc 无注解零入侵基于java注释<br />
-          * 工具类框架 Hutool、Lombok 减少代码冗余 增加安全性<br />
-          * 代码生成器 适配MP、SpringDoc规范化代码 一键生成前后端代码<br />
-          * 部署方式 Docker 容器编排 一键部署业务集群<br />
-          * 国际化 SpringMessage Spring标准国际化方案<br />
-        </p>
-        <p><b>当前版本:</b> <span>v5.2.1</span></p>
-        <p>
-          <el-tag type="danger">&yen;免费开源</el-tag>
-        </p>
-        <p>
-          <el-button type="primary" icon="Cloudy" plain @click="goTarget('https://gitee.com/dromara/RuoYi-Vue-Plus')">访问码云</el-button>
-          <el-button type="primary" icon="Cloudy" plain @click="goTarget('https://github.com/dromara/RuoYi-Vue-Plus')">访问GitHub</el-button>
-          <el-button type="primary" icon="Cloudy" plain @click="goTarget('https://plus-doc.dromara.org/#/ruoyi-vue-plus/changlog')"
-            >更新日志</el-button
-          >
-        </p>
+  <div class="home">
+    <!-- 标题 -->
+    <div id="curtain">
+      <el-row justify="center" align="middle" style="height: 50px; background-color:cornflowerblue;">
+        <el-col :span="24" >
+          <h1 class="centered-title" align="center" style="color:aliceblue">云时代数据中心</h1>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 轮播器 -->
+    <el-carousel :interval="4000" type="card" height="300px" style="margin-top: 20px;">
+      <el-carousel-item>
+        <a href="https://www.baidu.com" target="_blank">
+          <img src="../assets/images/主页轮播器1.jpg" alt="Image 1" style="width: 100%;">
+        </a>
+      </el-carousel-item>
+      <el-carousel-item>
+        <a href="https://www.jd.com" target="_blank">
+          <img src="../assets/images/主页轮播器2.jpg" alt="Image 2" style="width: 100%;">
+        </a>
+      </el-carousel-item>
+      <el-carousel-item>
+        <a href="https://www.taobao.com" target="_blank">
+          <img src="../assets/images/主页轮播器3.jpg" alt="Image 3" style="width: 100%;">
+        </a>
+      </el-carousel-item>
+    </el-carousel>
+    <!-- 公告和成员信息 -->
+    <el-row style="margin-top: 20px;" >
+      <!-- 公告 -->
+      <el-col :span="12">
+        <el-card shadow="hover" class="scrollbar-card" style="height: 290px;">
+          <p slot="header" align="center" style="color:#333;font-weight: bold">通知公告</p>
+          <el-table v-loading="loading" :data="noticeList">
+            <el-table-column v-if="false" label="序号" align="center" prop="noticeId" width="100" />
+            <el-table-column label="公告标题" align="center" prop="noticeTitle" :show-overflow-tooltip="true" />
+            <el-table-column label="公告类型" align="center" prop="noticeType" width="100">
+              <template #default="scope">
+                <dict-tag :options="sys_notice_type" :value="scope.row.noticeType" />
+              </template>
+            </el-table-column>
+            <el-table-column label="创建时间" align="center" prop="createTime" width="100">
+              <template #default="scope">
+                <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d}') }}</span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
       </el-col>
-
-      <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>RuoYi-Cloud-Plus多租户微服务管理系统</h2>
-        <p>
-          RuoYi-Cloud-Plus 微服务通用权限管理系统 重写 RuoYi-Cloud 全方位升级(不兼容原框架)
-          <br />
-          * 前端开发框架 Vue3、TS、Element UI<br />
-          * 后端开发框架 Spring Boot<br />
-          * 微服务开发框架 Spring Cloud、Spring Cloud Alibaba<br />
-          * 容器框架 Undertow 基于 XNIO 的高性能容器<br />
-          * 权限认证框架 Sa-Token、Jwt 支持多终端认证系统<br />
-          * 关系数据库 MySQL 适配 8.X 最低 5.7<br />
-          * 关系数据库 Oracle 适配 11g 12c<br />
-          * 关系数据库 PostgreSQL 适配 13 14<br />
-          * 关系数据库 SQLServer 适配 2017 2019<br />
-          * 缓存数据库 Redis 适配 6.X 最低 5.X<br />
-          * 分布式注册中心 Alibaba Nacos 采用2.X 基于GRPC通信高性能<br />
-          * 分布式配置中心 Alibaba Nacos 采用2.X 基于GRPC通信高性能<br />
-          * 服务网关 Spring Cloud Gateway 响应式高性能网关<br />
-          * 负载均衡 Spring Cloud Loadbalancer 负载均衡处理<br />
-          * RPC远程调用 Apache Dubbo 原生态使用体验、高性能<br />
-          * 分布式限流熔断 Alibaba Sentinel 无侵入、高扩展<br />
-          * 分布式事务 Alibaba Seata 无侵入、高扩展 支持 四种模式<br />
-          * 分布式消息队列 Apache Kafka 高性能高速度<br />
-          * 分布式消息队列 Apache RocketMQ 高可用功能多样<br />
-          * 分布式消息队列 RabbitMQ 支持各种扩展插件功能多样性<br />
-          * 分布式搜索引擎 ElasticSearch 业界知名<br />
-          * 分布式链路追踪 Apache SkyWalking 链路追踪、网格分析、度量聚合、可视化<br />
-          * 分布式日志中心 ELK 业界成熟解决方案<br />
-          * 分布式监控 Prometheus、Grafana 全方位性能监控<br />
-          * 其余与 Vue 版本一致<br />
-        </p>
-        <p><b>当前版本:</b> <span>v2.2.0</span></p>
-        <p>
-          <el-tag type="danger">&yen;免费开源</el-tag>
-        </p>
-        <p>
-          <el-button type="primary" icon="Cloudy" plain @click="goTarget('https://gitee.com/dromara/RuoYi-Cloud-Plus')">访问码云</el-button>
-          <el-button type="primary" icon="Cloudy" plain @click="goTarget('https://github.com/dromara/RuoYi-Cloud-Plus')">访问GitHub</el-button>
-          <el-button type="primary" icon="Cloudy" plain @click="goTarget('https://plus-doc.dromara.org/#/ruoyi-cloud-plus/changlog')"
-            >更新日志</el-button
-          >
-        </p>
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <p slot="header" align="center" style="font-size: 14; color:#333;font-weight: bold">成员信息</p>
+          <el-row>
+            <!-- 学历分布柱状图 -->
+            <el-col :span="12" >
+              <div style="height: 200px; width: 100%;" ref="educationChart"></div>
+            </el-col>
+            <!-- 职称分布柱状图 -->
+            <el-col :span="12" style="margin-left: -5px;">
+              <div style="height: 200px; width: 100%;" ref="titleChart"></div>
+            </el-col>
+          </el-row>
+        </el-card>
       </el-col>
     </el-row>
-    <el-divider />
+    <!-- 项目统计和知识产权 -->
+    <el-row style="margin-top: 20px;">
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <div style="height: 300px;" ref="projectChart"></div>
+        </el-card>
+        </el-col>
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <div style="height: 300px;" ref="resultChart"></div>
+        </el-card>
+      </el-col>
+    </el-row>
+            
   </div>
 </template>
 
 <script setup name="Index" lang="ts">
-const goTarget = (url: string) => {
-  window.open(url, '__blank');
+import { listNotice } from '@/api/system/notice';
+import { NoticeForm, NoticeQuery, NoticeVO } from '@/api/system/notice/types';
+import request from '@/utils/request';
+import * as echarts from 'echarts';
+
+const { proxy } = getCurrentInstance() as ComponentInternalInstance;
+const { sys_notice_type } = toRefs<any>(proxy?.useDict('sys_notice_type'));
+const noticeList = ref<NoticeVO[]>([]);
+const loading = ref(true);
+const total = ref(0);
+const educationData = ref({ categories: [], data: [] });
+const jobtitleData = ref({ categories: [], data: [] });
+const projectListData = ref(null);
+const typeData = ref({ categories: [], data: [] });
+const educationChart = ref(null);
+const titleChart = ref(null);
+const projectChart = ref(null);
+const resultChart = ref(null);
+
+
+const initFormData: NoticeForm = {
+  noticeId: undefined,
+  noticeTitle: '',
+  noticeType: '',
+  noticeContent: '',
+  status: '0',
+  remark: '',
+  createByName: ''
 };
+const data = reactive<PageData<NoticeForm, NoticeQuery>>({
+  form: { ...initFormData },
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    noticeTitle: '',
+    createByName: '',
+    status: '',
+    noticeType: ''
+  },
+  rules: {
+    noticeTitle: [{ required: true, message: '公告标题不能为空', trigger: 'blur' }],
+    noticeType: [{ required: true, message: '公告类型不能为空', trigger: 'change' }]
+  }
+});
+
+const { queryParams } = toRefs(data);
+
+/** 获取公告数据 */
+const getList = async () => {
+  loading.value = true;
+  const res = await listNotice(queryParams.value);
+  noticeList.value = res.rows.slice(0, 4); // 获取前五条通知
+  total.value = res.total;
+  loading.value = false;
+};
+/** 获取学历数据 */
+const getDiplomaData = async (callback?: () => void) => {
+  try {
+    const resp = await request({
+      url: '/statistic/user/diploma',
+      method: 'get',
+      data: {},
+    });
+    educationData.value = {
+      categories: Object.keys(resp),
+      data: Object.values(resp),
+    };
+    if (callback && typeof callback === 'function') {
+      callback();
+    }
+  } catch (error) {
+    console.error('Failed to fetch project list data:', error);
+  }
+};
+/** 获取职称数据 */
+const getJobtitleData = async (callback?: () => void) => {
+  try {
+    const resp = await request({
+      url: '/statistic/user/jobtitle',
+      method: 'get',
+      data: {},
+    });
+    jobtitleData.value = {
+      categories: Object.keys(resp),
+      data: Object.values(resp),
+    };
+    if (callback && typeof callback === 'function') {
+      callback();
+    }
+  } catch (error) {
+    console.error('Failed to fetch project list data:', error);
+  }
+};
+/** 获取项目统计数据 */
+const getProjectListData = async (callback?: () => void) => {
+  try {
+    const resp = await request({
+      url: '/statistic/project/level',
+      method: 'get',
+      data: {},
+    });
+    projectListData.value = resp;
+    if (callback && typeof callback === 'function') {
+      callback();
+    }
+  } catch (error) {
+    console.error('Failed to fetch project list data:', error);
+  }
+};
+/** 获取知识产权数据 */
+const getTypeData = async (callback?: () => void) => {
+  try {
+    const resp = await request({
+      url: '/statistic/ip/type',
+      method: 'get',
+      data: {},
+    });
+    typeData.value = {
+      categories: Object.keys(resp),
+      data: Object.values(resp),
+    };
+    if (callback && typeof callback === 'function') {
+      callback();
+    }
+  } catch (error) {
+    console.error('Failed to fetch type data:', error);
+  }
+};
+
+const initEducationChart = () => {
+  // 获取学历图表容器
+  const educationChartInstance = echarts.init(educationChart.value);
+  // 将数据转换为 ECharts 饼图所需的格式
+  const pieData = educationData.value.data.map((count, index) => ({
+    value: count,
+    name: educationData.value.categories[index],
+  }));
+  // 配置饼图选项
+  const option = {
+    backgroundColor: 'rgba(0, 0, 0, 0)', // 设置背景为透明
+    title: {
+      text: '学历',
+      left: 'center',
+      textStyle: {
+        color: '#333',
+        fontSize: 16,
+      },
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} 人 ({d}%)',
+    },
+    series: [{
+      name: '学历数量',
+      type: 'pie',
+      radius: ['40%', '60%'],
+      center: ['50%', '50%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: true,
+        position: 'outside',
+        formatter: '{b}: \n{c} 人',
+        rotate: 45, // 设置文字旋转角度
+        fontSize: 9 // 设置字体大小
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: '14',
+          fontWeight: 'bold',
+        },
+      },
+      labelLine: {
+        show: true,
+        length2: 10,
+      },
+      data: pieData,
+    }],
+  };
+
+  // 设置饼图的选项
+  educationChartInstance.setOption(option);
+        // 监听窗口大小变化
+        window.addEventListener('resize', () => {
+        educationChartInstance.resize();
+      });
+};
+
+const initTitleChart = () => {
+  // 获取职称图表容器
+  const titleChartInstance = echarts.init(titleChart.value);
+
+  // 将数据转换为 ECharts 实心饼图所需的格式
+  const pieData = jobtitleData.value.data.map((count, index) => ({
+    value: count,
+    name: jobtitleData.value.categories[index],
+  }));
+
+  // 配置实心饼图选项
+  const option = {
+    backgroundColor: 'rgba(0, 0, 0, 0)', // 设置背景为透明
+    title: {
+      text: '职称',
+      left: 'center',
+      textStyle: {
+        color: '#333',
+        fontSize: 16,
+      },
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} 人 ({d}%)',
+    },
+    series: [{
+      name: '职称数量',
+      type: 'pie',
+      radius: '60%',
+      center: ['50%', '50%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: true,
+        position: 'outside',
+        formatter: '{b}:\n {c} 人',
+        rotate: 45, // 设置文字旋转角度
+        fontSize: 9 // 设置字体大小
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: '14',
+          fontWeight: 'bold',
+        },
+      },
+      labelLine: {
+        show: true,
+        length2: 10,
+      },
+      data: pieData,
+    }],
+  };
+
+  // 设置实心饼图的选项
+  titleChartInstance.setOption(option);
+};
+
+const initProjectChart = () => {
+  // 获取项目列表图表容器
+  const projectChartInstance = echarts.init(projectChart.value);
+  // 将数据转换为 ECharts 柱状图所需的格式
+  const barData = Object.entries(projectListData.value).map(([category, count]) => ({
+    value: count,
+    name: category,
+  }));
+  const option = {
+    title: {
+      text: '项目数量', // 设置标题文本
+      left: 'center', // 标题居中显示
+      textStyle: {
+        color: '#333', // 标题颜色
+        fontSize: 16, // 标题字体大小
+      },
+    },
+    xAxis: {
+      type: 'category',
+      data: Object.keys(projectListData.value), // 使用项目类别作为横坐标数据
+      axisLabel: {
+        interval: 0,
+        rotate: -45, // 将横坐标文字逆时针旋转45度，适应柱状图显示
+      },
+    },
+    yAxis: {
+      type: 'value',
+    },
+    tooltip: {
+      trigger: 'axis',
+      formatter: '{b}: {c} 个',
+    },
+    series: [{
+      type: 'bar',
+      data: barData,
+      itemStyle: {
+        color: "cornflowerblue", // 可以使用 getRandomColor 方法为每个柱设置不同的颜色
+      },
+    }],
+  };
+  projectChartInstance.setOption(option);
+    // 监听窗口大小变化
+  window.addEventListener('resize', () => {
+    projectChartInstance.resize();
+    });
+};
+
+const initResultChart = () => {
+  // 获取知识产权图表容器
+  const resultChartInstance = echarts.init(resultChart.value);
+  // 将数据转换为 ECharts 柱状图所需的格式
+  const barData = typeData.value.data.map((count, index) => ({
+    value: count,
+    name: typeData.value.categories[index],
+  }));
+  // 配置柱状图选项
+  const option = {
+    title: {
+      text: '知识产权', // 设置标题文本
+      left: 'center', // 标题居中显示
+      textStyle: {
+        color: '#333', // 标题颜色
+        fontSize: 16, // 标题字体大小
+      },
+    },
+    xAxis: {
+      type: 'category',
+      data: typeData.value.categories, // 使用知识产权类别作为横坐标数据
+      axisLabel: {
+        interval: 0,
+        rotate: -45, // 将横坐标文字逆时针旋转45度，适应柱状图显示
+      },
+    },
+    yAxis: {
+      type: 'value',
+    },
+    tooltip: {
+      trigger: 'axis',
+      formatter: '{b}: {c} 个',
+    },
+    series: [{
+      type: 'bar',
+      data: barData,
+      itemStyle: {
+        color: "cornflowerblue", 
+      },
+    }],
+  };
+  // 设置柱状图的选项
+  resultChartInstance.setOption(option);
+  // 监听窗口大小变化
+  window.addEventListener('resize', () => {
+    resultChartInstance.resize();
+      });
+};
+
+// 监听窗口的 resize 事件
+const handleResize = () => {
+  // 重新初始化图表
+  initEducationChart();
+  initTitleChart();
+  initProjectChart();
+  initResultChart();
+};
+// 异步
+const fetchData = async () => {
+  await Promise.all([getList(), getDiplomaData(), getJobtitleData(), getProjectListData(), getTypeData()]);
+  initEducationChart();
+  initTitleChart();
+  initProjectChart();
+  initResultChart();
+};
+
+onMounted(() => {
+  fetchData();
+  window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
 </script>
-
-<style scoped lang="scss">
-.home {
-  blockquote {
-    padding: 10px 20px;
-    margin: 0 0 20px;
-    font-size: 17.5px;
-    border-left: 5px solid #eee;
-  }
-  hr {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    border: 0;
-    border-top: 1px solid #eee;
-  }
-  .col-item {
-    margin-bottom: 20px;
-  }
-
-  ul {
-    padding: 0;
-    margin: 0;
-  }
-
-  font-family: 'open sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  font-size: 13px;
-  color: #676a6c;
-  overflow-x: hidden;
-
-  ul {
-    list-style-type: none;
-  }
-
-  h4 {
-    margin-top: 0px;
-  }
-
-  h2 {
-    margin-top: 10px;
-    font-size: 26px;
-    font-weight: 100;
-  }
-
-  p {
-    margin-top: 10px;
-
-    b {
-      font-weight: 700;
-    }
-  }
-
-  .update-log {
-    ol {
-      display: block;
-      list-style-type: decimal;
-      margin-block-start: 1em;
-      margin-block-end: 1em;
-      margin-inline-start: 0;
-      margin-inline-end: 0;
-      padding-inline-start: 40px;
-    }
-  }
-}
-</style>
