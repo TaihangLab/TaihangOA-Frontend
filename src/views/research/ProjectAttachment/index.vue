2e87d58d2b8b4df309f5ef04117658463e99ab3f
Pasiner <1169983244@qq.com>
2024-08-06 16:35:36 +0800

    科研管理的项目附件

--- src/views/research/ProjectAttachment/index.vue
+++ src/views/research/ProjectAttachment/index.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="p-2">
-    <transition :enter-active-class="proxy?.animate.searchAnimate.enter" :leave-active-class="proxy?.animate.searchAnimate.leave">
-      <div v-show="showSearch" class="mb-[10px]">
-        <el-card shadow="hover">
-          <el-form ref="queryFormRef" :model="queryParams" :inline="true">
-            <el-form-item label="项目名称" prop="roleName">
-              <el-input v-model="queryParams.roleName" placeholder="请选择项目" clearable @keyup.enter="handleQuery" />
-            </el-form-item>
+    <transition :enter-active-class="proxy?.animate.searchAnimate.enter" :leave-active-class="proxy?.animate.searchAnimate.leave"></transition>
+    <div v-show="showSearch" class="mb-[10px]">
+      <el-card shadow="hover">
+        <el-form ref="dataFormRef" :model="queryParams" :inline="true" class="demo-form-inline" style="margin-left: 30px; margin-top: 20px;">
 
-            <el-form-item>
-              <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
-              <el-button icon="Refresh" @click="resetQuery">重置</el-button>
-            </el-form-item>
-          </el-form>
-        </el-card>
-      </div>
-    </transition>
+          <el-form-item label="项目名称" prop="ProjectName">
+            <el-cascader v-model="responsibleproject" :options="projecttree" clearable :show-all-levels="false"
+              placeholder="请选择项目" @keyup.enter.native="handleQuery"></el-cascader>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" icon="Search"  @click="handleQuery">搜索</el-button>
+            <el-button icon="Refresh"  @click="resetQuery">重置</el-button>
+          </el-form-item>
+
+        </el-form>
+      </el-card>
+    </div>
 
     <el-card shadow="hover">
+      
       <template #header>
         <el-row :gutter="10">
           <el-col :span="1.5">
@@ -27,192 +29,173 @@
         </el-row>
       </template>
 
-      <el-table ref="roleTableRef" v-loading="loading" border :data="roleList" @selection-change="handleSelectionChange">
-        <el-table-column type="selection" width="55" align="center" />
-        <el-table-column label="文件名称" fixed="left" prop="roleName" :show-overflow-tooltip="true" min-width="20%" />
-        <el-table-column label="文件格式" prop="roleName" :show-overflow-tooltip="true" min-width="20%" />
-        <el-table-column label="上传人" prop="roleName" :show-overflow-tooltip="true" min-width="20%" />
-        <el-table-column label="所属大事记" prop="roleName" :show-overflow-tooltip="true" min-width="20%" />
-        <el-table-column label="文件上传时间" prop="roleName" :show-overflow-tooltip="true" min-width="15%" />
+      <div>
+        <el-table ref="multipleTable" :data="attachmentslist" border style="width: 100%"
+          :row-style="{ height: '50px' }" :cell-style="{ padding: '0px' }">
+          <el-table-column type="selection" width="50" align="center" />
+          <el-table-column label="文件名称" :resizable="false" align="center" :show-overflow-tooltip="true"
+            width="300">
+            <template #default="scope">
+              {{ truncatedName(scope.row.originalName) }}
+            </template>
+          </el-table-column>
+          <el-table-column :label="'文件格式'" align="center" prop="fileSuffix" :show-overflow-tooltip="true"
+            width="120">
+            <template #default="scope">
+              <div width="120">
+                <el-tag v-if="scope.row.fileSuffix === '.pdf'" type="success" >{{
+                  scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.docx'" type="warning" >{{
+                  scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.doc'" type="warning" >{{
+                  scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.xls'" >{{ scope.row.fileSuffix
+                }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.xlsx'" >{{ scope.row.fileSuffix
+                }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.ppt'" type="danger" >{{
+                  scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else-if="scope.row.fileSuffix === '.pptx'" type="danger" >{{
+                  scope.row.fileSuffix }}</el-tag>
+                <el-tag v-else type="info" >{{ scope.row.fileSuffix }}</el-tag>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="上传人" :resizable="false" align="center" prop="createBy" width="120">
+          </el-table-column>
+          <el-table-column label="所属大事记" :resizable="false" align="center" prop="milestoneTitle" width="300">
+          </el-table-column>
+          <el-table-column label="文件上传时间" :resizable="false" align="center" prop="createTime" width="170">
+          </el-table-column>
+          <el-table-column :label="'操作'" :resizable="false" align="center" min-width="100px" fixed="right">
+            <template #default="scope">
+              <el-button  type="text" icon="el-icon-download" @click="handleDownload(scope.row)" v-hasPermi="['project:oss:download']">下载</el-button>
+            </template>
+          </el-table-column>
 
-        <el-table-column fixed="right" label="操作" min-width="5%">
-          <template #default="scope">
-            <el-tooltip v-if="scope.row.roleId !== 0" content="下载" placement="top">
-              <el-button v-hasPermi="['system:role:edit']" link type="primary" icon="Download" @click="handleUpdate(scope.row)"></el-button>
-            </el-tooltip>
-          </template>
-        </el-table-column>
-      </el-table>
-
-      <pagination
-        v-if="total > 0"
-        v-model:total="total"
-        v-model:page="queryParams.pageNum"
-        v-model:limit="queryParams.pageSize"
-        @pagination="getList"
-      />
+        </el-table>
+      </div>
+      <el-pagination :current-page="queryParam.pageNum" :page-size="queryParam.pageSize"
+        :page-sizes="[5, 10, 20, 50, 100]" :total="total" layout="total ,sizes,prev,pager,next,jumper"
+        style="margin-top: 30px" @size-change="sizeChangeHandle" @current-change="CurrentChangeHandle">
+      </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { addRole, changeRoleStatus, dataScope, delRole, getRole, listRole, updateRole, deptTreeSelect } from '@/api/system/role';
-import { roleMenuTreeselect, treeselect as menuTreeselect } from '@/api/system/menu/index';
-import { RoleVO, RoleForm, RoleQuery, DeptTreeOption } from '@/api/system/role/types';
-import { MenuTreeOption, RoleMenuTree } from '@/api/system/menu/types';
+import { ref, reactive, onMounted } from 'vue';
+import request from '@/utils/request';
 
 const { proxy } = getCurrentInstance() as ComponentInternalInstance;
-
-const roleList = ref<RoleVO[]>();
-const loading = ref(true);
-const showSearch = ref(true);
-const ids = ref<Array<string | number>>([]);
-const single = ref(true);
-const multiple = ref(true);
+const responsibleproject = ref([]);
+const dataFormRef = ref<ElFormInstance>();
 const total = ref(0);
-const dateRange = ref<[DateModelType, DateModelType]>(['', '']);
-const menuOptions = ref<MenuTreeOption[]>([]);
-const menuExpand = ref(false);
-const menuNodeAll = ref(false);
-const deptExpand = ref(true);
-const deptNodeAll = ref(false);
-const deptOptions = ref<DeptTreeOption[]>([]);
 
-/** 数据范围选项*/
-
-const queryFormRef = ref<ElFormInstance>();
-const roleFormRef = ref<ElFormInstance>();
-const dataScopeRef = ref<ElFormInstance>();
-const menuRef = ref<ElTreeInstance>();
-const deptRef = ref<ElTreeInstance>();
-
-const initForm: RoleForm = {
-  roleId: undefined,
-  roleSort: 1,
-  status: '0',
-  roleName: '',
-  roleKey: '',
-  menuCheckStrictly: true,
-  deptCheckStrictly: true,
-  remark: '',
-  dataScope: '1',
-  menuIds: [],
-  deptIds: []
-};
-
-const data = reactive<PageData<RoleForm, RoleQuery>>({
-  form: { ...initForm },
-  queryParams: {
-    pageNum: 1,
-    pageSize: 10,
-    roleName: '',
-    roleKey: '',
-    status: ''
-  },
-  rules: {
-    roleName: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }],
-    roleKey: [{ required: true, message: '权限字符不能为空', trigger: 'blur' }],
-    roleSort: [{ required: true, message: '角色顺序不能为空', trigger: 'blur' }]
-  }
+const attachmentslist = ref([]);
+const showSearch = ref(true);
+const queryParams = reactive({
+  projectId: undefined,
 });
-const { form, queryParams, rules } = toRefs(data);
-
-const dialog = reactive<DialogOption>({
-  visible: false,
-  title: ''
+const queryParam = reactive({
+  pageNum: 1,
+  pageSize: 10,
 });
+const header = reactive({});
+const projecttree = ref(undefined);
 
-/**
- * 查询角色列表
- */
-const getList = () => {
-  loading.value = true;
-  listRole(proxy?.addDateRange(queryParams.value, dateRange.value)).then((res) => {
-    roleList.value = res.rows;
-    total.value = res.total;
-    loading.value = false;
-  });
+
+/** 获取附件名称 */
+const truncatedName = (originalName: string) => {
+  const lastDotIndex = originalName.lastIndexOf('.');
+  return lastDotIndex !== -1 ? originalName.substring(0, lastDotIndex) : originalName;
 };
 
-/**
- * 搜索按钮操作
- */
-const handleQuery = () => {
-  queryParams.value.pageNum = 1;
-  getList();
+/** 查询附件列表 */
+const getAttachmentsList = async () => {
+  await checkattachments();
+  await getProjectTree();
 };
 
-/** 重置 */
-const resetQuery = () => {
-  dateRange.value = ['', ''];
-  queryFormRef.value?.resetFields();
-  handleQuery();
+/** 获取数据*/
+const fetchData = () => {
+  getAttachmentsList();
 };
 
-/** 导出按钮操作 */
-const handleExport = () => {
-  proxy?.download(
-    'system/role/export',
-    {
-      ...queryParams.value
-    },
-    `role_${new Date().getTime()}.xlsx`
-  );
-};
-/** 多选框选中数据 */
-const handleSelectionChange = (selection: RoleVO[]) => {
-  ids.value = selection.map((item: RoleVO) => item.roleId);
-  single.value = selection.length != 1;
-  multiple.value = !selection.length;
-};
-
-/** 查询菜单树结构 */
-const getMenuTreeselect = async () => {
-  const res = await menuTreeselect();
-  menuOptions.value = res.data;
-};
-/** 重置新增的表单以及其他数据  */
-const reset = () => {
-  menuRef.value?.setCheckedKeys([]);
-  menuExpand.value = false;
-  menuNodeAll.value = false;
-  deptExpand.value = true;
-  deptNodeAll.value = false;
-  form.value = { ...initForm };
-  roleFormRef.value?.resetFields();
-};
-
-/** 添加角色 */
-const handleAdd = () => {
-  reset();
-  getMenuTreeselect();
-  dialog.visible = true;
-  dialog.title = '添加角色';
-};
-/** 修改角色 */
-const handleUpdate = async (row?: RoleVO) => {
-  reset();
-  const roleId = row?.roleId || ids.value[0];
-  const { data } = await getRole(roleId);
-  Object.assign(form.value, data);
-  form.value.roleSort = Number(form.value.roleSort);
-  const res = await getRoleMenuTreeselect(roleId);
-  dialog.title = '修改角色';
-  dialog.visible = true;
-  res.checkedKeys.forEach((v) => {
-    nextTick(() => {
-      menuRef.value?.setChecked(v, true, false);
+//获取全部附件列表
+const checkattachments = async () => {
+  try {
+    const resp = await request({
+      url: '/milestone/oss/getAllList',
+      method: 'post',
+      data: queryParams,
+      params: queryParam,
     });
-  });
+    attachmentslist.value = resp.rows;
+    total.value = resp.total;
+
+  } catch (error) {
+    console.error('获取附件列表时出错：', error);
+  }
 };
-/** 根据角色ID查询菜单树结构 */
-const getRoleMenuTreeselect = (roleId: string | number) => {
-  return roleMenuTreeselect(roleId).then((res): RoleMenuTree => {
-    menuOptions.value = res.data.menus;
-    return res.data;
-  });
+//获取项目树
+const getProjectTree = async () => {
+  try {
+    const resp = await request({
+      url: '/ip/getProjectTree',
+      method: 'get',
+      params: header,
+    });
+    projecttree.value = resp.data;
+    console.log(attachmentslist )
+  console.log(proxy)
+  
+
+  } catch (error) {
+    console.error('获取项目树时出错：', error);
+  }
 };
+
+/** 搜索按钮操作 */
+const handleQuery = () => {
+  queryParams.projectId = responsibleproject.value[responsibleproject.value.length - 1];
+  getAttachmentsList();
+};
+
+/** 重置按钮操作 */
+const resetQuery = () => {
+  responsibleproject.value = [];
+  queryParams.projectId = undefined;
+  queryParam.pageNum = 1;
+  queryParam.pageSize = 10;
+  checkattachments();
+};
+
+/** 每页显示多少条 */
+const sizeChangeHandle = (val: number) => {
+  queryParam.pageSize = val;
+  fetchData();
+};
+
+/** 改变页数 */
+const CurrentChangeHandle = (val: number) => {
+  queryParam.pageNum = val;
+  fetchData();
+};
+
+// 下载按钮操作
+const handleDownload = function(row: any) {
+  proxy?.$download.oss(row.ossId);
+};
+
 onMounted(() => {
-  getList();
+  getAttachmentsList();
 });
 </script>
+
+<style>
+.box-card {
+  width: 100%;
+  margin-bottom: 20px;
+}
+</style>
