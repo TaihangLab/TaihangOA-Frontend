8c39e17f51cfcd4fb52ed9f0d40190ab53bcb567
范佳兴 <c1111111c@163.com>
2024-08-19 09:02:39 +0800

    feat:新增支出导入时相关功能

--- src/views/project/components/Funds/ExpenditureImport.vue
+++ src/views/project/components/Funds/ExpenditureImport.vue
@@ -1,11 +1,8 @@
 <template>
-  <el-dialog :model-value="visible" title="信息导入" width="400px" append-to-body @close="closeExpenselImportDialog">
+  <el-dialog :model-value="visible" title="支出信息导入" width="350px" append-to-body @close="closeExpenselImportDialog">
     <div style="padding: 20px">
-      <!-- 显示已选择的文件 -->
-      <div v-if="fileTemp" class="selected-file">已选择文件：{{ fileTemp.name }}</div>
       <el-upload
-        ref="upload"
-        class="upload-demo"
+        ref="uploadRef"
         action=""
         :on-change="handleChange"
         :on-remove="handleRemove"
@@ -25,34 +22,36 @@
           <div class="el-upload__tip">仅允许导入xls、xlsx格式文件。</div>
         </template>
       </el-upload>
-      <div style="margin-top: 5px">
-        <el-button size="small" @click="handleCancel">取消</el-button>
-        <el-button size="small" type="primary" :disabled="!fileTemp" @click="confirmUpload">确认</el-button>
+      <div style="margin-top: 20px">
+        <el-button @click="handleCancel" type="warning">下载模版</el-button>
+        <el-button @click="handleCancel">取消</el-button>
+        <el-button type="primary" :disabled="!fileTemp" @click="confirmUpload">确认</el-button>
       </div>
     </div>
   </el-dialog>
 </template>
 
-<script setup lang="ts">
+<script setup Name="ExpenditureImport" lang="ts">
 import { defineProps, defineEmits } from 'vue';
+import { importExpenditureData } from '@/api/project/funds';
 
 const props = defineProps<{ visible: boolean }>();
-const emits = defineEmits(['update:visible', 'close:visible']);
+const emits = defineEmits(['new-data', 'update:visible', 'close:visible']);
 const fileTemp = ref(null); // 存储用户选择的文件
-
+const uploadRef = ref<ElUploadInstance>();
 const closeExpenselImportDialog = () => {
   emits('close:visible', false);
+  uploadRef.value?.clearFiles();
 };
 
 // 超出最大上传文件数量处理方法
 const handleExceed = () => {
-  // 提示超出最大上传文件数量
+  ElMessage.warning('只能上传一个文件');
 };
 
 // 移除文件操作方法
 const handleRemove = (file: any, fileList: any[]) => {
   fileTemp.value = null;
-  // 删除已上传的文件
 };
 
 // 文件选择变化操作方法
@@ -64,25 +63,26 @@ const handleChange = (file: any, fileList: any[]) => {
 // 取消上传操作方法
 const handleCancel = () => {
   fileTemp.value = null;
-  // 关闭导入对话框
   emits('close:visible', false);
 };
 
 // 确认上传操作方法
 const confirmUpload = () => {
-  if (fileTemp.value) {
-    const formData = new FormData();
-    formData.append('file', fileTemp.value);
-    request
-      .post('/project/funds/importData', formData)
-      .then((response) => {
-        // 处理上传成功的响应
-      })
-      .catch((error) => {
-        // 处理上传失败的情况
-      });
-  } else {
-    // 提示未选择文件
+  if (!fileTemp.value) {
+    ElMessage.error('请先选择一个文件');
+    return;
   }
+  importExpenditureData(fileTemp.value)
+    .then((response) => {
+      emits('close:visible', false);
+      emits('new-data', response.data)
+    })
+    .catch(error => {
+      fileTemp.value = null;
+      ElMessage.error('上传失败，请重试');
+    })
+    .finally(() => {
+      fileTemp.value = null;
+    });
 };
 </script>
