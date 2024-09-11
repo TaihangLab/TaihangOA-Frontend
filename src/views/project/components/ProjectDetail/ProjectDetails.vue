c51c345c72e606510863433cdb873f86e50b7076
范佳兴 <c1111111c@163.com>
2024-09-11 16:28:03 +0800

    refactor:项目详情查看代码重构

--- src/views/project/components/ProjectDetail/ProjectDetails.vue
+++ src/views/project/components/ProjectDetail/ProjectDetails.vue
@@ -5,7 +5,7 @@
         <el-tab-pane label="基本信息" name="first">
           <div style="margin-top: 5px"></div>
           <el-descriptions-item label="基本信息" :span="2" />
-          <el-descriptions class="margin-top" title="" :column="2" :label-style="{ width: '25%' }" :content-style="{ width: '25%' }" border>
+          <el-descriptions class="margin-top" title="" :column="2" :label-style="{ width: '25%' }" :content-style="{ width: '25%' }" border v-loading="loading" :data="projectDetails">
             <el-descriptions-item label="项目名称">{{ projectDetails.projectInfoVO.assignedSubjectName }}</el-descriptions-item>
             <el-descriptions-item label="项目任务书编号">{{ projectDetails.projectInfoVO.projectAssignmentSerialNo }}</el-descriptions-item>
             <el-descriptions-item label="负责课题">{{ projectDetails.projectInfoVO.assignedSubjectSection }}</el-descriptions-item>
@@ -38,7 +38,7 @@
                 <span style="font-size: 14px">项目简介</span>
               </template>
               <div class="unselectable-textbox" style="font-size: 14px; color: #606266">
-                {{ projectDescription }}
+                {{ projectDetails.projectInfoVO.projectDescription }}
               </div>
             </el-collapse-item>
           </el-collapse>
@@ -117,7 +117,7 @@
         </el-tab-pane>
         <el-tab-pane label="项目申报附件" name="sixth">
           <div style="margin-top: 5px"></div>
-          <el-table v-loading="loading" :data="transformedFile" style="margin: 0; padding: 0">
+          <el-table v-loading="loading" :data="projectDetails.projectAttachmentVOList" style="margin: 0; padding: 0">
             <el-table-column :label="'文件名称'" align="center" prop="originalName" :show-overflow-tooltip="true" />
             <el-table-column :label="'文件格式'" align="center" prop="fileSuffix" :show-overflow-tooltip="true">
               <template #default="scope">
@@ -200,13 +200,16 @@
 import { defineProps, defineEmits, watch } from 'vue';
 import ProjectFunds from '@/views/project/components/ProjectDetail/ProjectFunds.vue';
 import { getProjectDetails } from '@/api/project/myProject/project';
-// 接收组件的props
+import { ProjectDetailsVO } from '@/api/project/myProject/types';
+
+
 const props = defineProps<{
   visible: boolean;
   projectId: string;
 }>();
 const emits = defineEmits(['update:visible']);
 const { proxy } = getCurrentInstance() as ComponentInternalInstance;
+const loading = ref(true);
 const { pro_cocompany_type, pro_level_type, pro_progress_status, pro_cooperative_unit, pro_user_role, sys_jobtitle_type, sys_diploma_type } =
   toRefs<any>(
     proxy?.useDict(
@@ -220,245 +223,50 @@ const { pro_cocompany_type, pro_level_type, pro_progress_status, pro_cooperative
     )
   );
 
-interface User {
-  nickName: string;
-  jobTitle: number;
-  diploma: number;
-  phonenumber: string;
-  deptName: string;
-  projectUserRoles: number[];
-}
+const projectDetails = reactive<ProjectDetailsVO>({
+  projectInfoVO: {},
+  projectPlanVOList: [],
+  projectTargetVOList: [],
+  projectUserVoList: [],
+  projectFundsVO: {},
+  projectAttachmentVOList: []
+});
 
-const loading = ref(true);
-const userList = ref<User[]>([]);
-
-interface ProjectDetails {
-  projectInfoVO: {
-    createDept: number; // 创建部门ID
-    createBy: number; // 创建人ID
-    createTime: string; // 创建时间
-    updateBy: number; // 更新人ID
-    updateTime: string; // 更新时间
-    projectId: number; // 项目ID
-    leadingUnit: string; // 项目牵头单位
-    assignedSubjectName: string; // 项目名称
-    projectContact: string; // 项目专员
-    projectAssignmentSerialNo: string; // 项目任务书编号
-    projectSource: string; // 项目来源
-    projectLevel: number; // 项目级别（0:省级, 1:国家级等）
-    hasLeadingRole: number; // 是否有牵头单位（1: 是, 0: 否）
-    assignedSubjectSection: string; // 负责课题
-    subjectAssignmentSerialNo: string; // 课题任务书编号
-    projectLeader: string; // 项目负责人
-    projectEstablishTime: string; // 项目立项时间
-    projectScheduledCompletionTime: string; // 项目计划完成时间
-    projectDuration: string; // 项目周期
-    projectDescription: string; // 项目简介
-    significanceAndNecessity?: string | null; // 项目意义和必要性
-    projectProgressStatus: number; // 项目进度状态（0: 未开始, 1: 进行中, 2: 完成）
-    completionProgress: string; // 完成进度
-    collaboratingUnit: string; // 合作单位
-    expertTeam: string; // 涉及专家团队
-    hasCooperativeUnit: number; // 是否有合作单位（1: 是, 0: 否）
-    awardDetails: string; // 获奖情况
-    publicationDetails: string; // 论文情况
-    patentDetails: string; // 专利情况
-    softwareCopyrightDetails: string; // 软件著作权情况
-    standardDetails: string; // 标准情况
-  };
-  projectUserVoList: Array<{
-    userId: number; // 用户ID
-    nickName: string; // 用户昵称
-    jobTitle: number; // 职位
-    diploma: number; // 学历
-    email: string; // 电子邮件
-    phonenumber: string; // 手机号码
-    deptName: string; // 部门名称
-    projectUserRoles: string[]; // 用户角色列表
-    userProjectNum: number; // 用户项目数量
-    userNationNum: number; // 国家级项目数量
-    userProvincialNum: number; // 省级项目数量
-    userEnterpriseNum: number; // 企业项目数量
-    userProjectNumNow: number; // 当前项目数量
-    userNationNumNow: number; // 当前国家级项目数量
-    userProvincialNumNow: number; // 当前省级项目数量
-    userEnterpriseNumNow: number; // 当前企业项目数量
-  }>;
-  projectTargetVOList: Array<{
-    targetName: string; // 指标名称
-    midtermTarget: string; // 中期指标值/状态
-    endTarget: string; // 完成指标值/状态
-  }>;
-  projectAttachmentVOList: Array<{
-    ossId: string; // OSS文件ID
-    fileName: string; // 文件名称
-    originalName: string; // 文件原始名称
-    fileSuffix: string; // 文件后缀
-    url: string; // 文件访问URL
-    createTime: string; // 创建时间
-    createBy: number; // 创建人ID
-    createByName: string; // 创建人名称
-    service: string; // 服务名
-    milestoneTitle?: string | null; // 里程碑标题（可选）
-  }>;
-  projectPlanVOList: Array<{
-    stageStartDate: string; // 阶段开始日期
-    stageEndDate: string; // 阶段结束日期
-    stageTask: string; // 阶段任务
-  }>;
-  projectFundsVO: Array<{ ProjectFundsV0List }>;
-}
-
-// 定义响应式引用
-const size = ref('');
-const border = ref(true);
-const activeName = ref('first');
 const activeNames = ref('first');
-
-const projectDescription = ref<any>();
-const projectLeaders = ref<any>();
-
-const defaultProjectDetails: ProjectDetails = {
-  projectInfoVO: {
-    createDept: undefined, // 默认值
-    createBy: undefined, // 默认值
-    createTime: '', // 默认值
-    updateBy: undefined, // 默认值
-    updateTime: '', // 默认值
-    projectId: undefined, // 默认值
-    leadingUnit: '', // 默认值
-    assignedSubjectName: '', // 默认值
-    projectContact: '', // 默认值
-    projectAssignmentSerialNo: '', // 默认值
-    projectSource: '', // 默认值
-    projectLevel: undefined, // 默认值
-    hasLeadingRole: undefined, // 默认值
-    assignedSubjectSection: '', // 默认值
-    subjectAssignmentSerialNo: '', // 默认值
-    projectLeader: '', // 默认值
-    projectEstablishTime: '', // 默认值
-    projectScheduledCompletionTime: '', // 默认值
-    projectDuration: '', // 默认值
-    projectDescription: '', // 默认值
-    significanceAndNecessity: null, // 默认值
-    projectProgressStatus: undefined, // 默认值
-    completionProgress: '', // 默认值
-    collaboratingUnit: '', // 默认值
-    expertTeam: '', // 默认值
-    hasCooperativeUnit: undefined, // 默认值
-    awardDetails: '', // 默认值
-    publicationDetails: '', // 默认值
-    patentDetails: '', // 默认值
-    softwareCopyrightDetails: '', // 默认值
-    standardDetails: '' // 默认值
-  },
-  projectUserVoList: [], // 默认值为空数组
-  projectTargetVOList: [], // 默认值为空数组
-  projectAttachmentVOList: [], // 默认值为空数组
-  projectPlanVOList: {
-    stageStartDate: '', // 阶段开始日期
-    stageEndDate: '', // 阶段结束日期
-    stageTask: '' // 阶段任务
-  },
-  projectFundsVO: []
-};
-const projectDetails = ref<ProjectDetails>(defaultProjectDetails);
-
-const projectMembers = () => {
-  getProjectDetails(props.projectId)
-    .then((resp) => {
-      userList.value = resp.data.projectUserVoList;
-      loading.value = false;
-    })
-    .catch(() => {
-      loading.value = false;
-    });
-};
-
-watch(() => props.projectId, projectMembers);
 
 // 定义methods
 const handleDownload = (row: { ossId: string }) => {
-  // 调用下载方法
   proxy?.$download.oss(row.ossId);
 };
 
-// 计算属性 transformedFile
-const transformedFile = computed(() => {
-  const result = projectDetails.value.projectAttachmentVOList.map((file) => {
-    const cleanedName = file.originalName.replace(/\.[^/.]+$/, '');
-    const createTime = new Date(file.createTime).toLocaleDateString();
-    return {
-      originalName: cleanedName,
-      fileSuffix: file.fileSuffix,
-      createBy: file.createBy,
-      createTime: createTime,
-      url: file.url,
-      ossId: file.ossId
-    };
-  });
-  return result;
-});
-
-// 监听projectdetails.value.projectAttachmentVOList属性的变化
-watch(
-  () => projectDetails.value.projectAttachmentVOList,
-  () => {
-    transformedFile.value;
-  },
-  { deep: true }
-);
-
-const transformedUserList = computed(() => {
-  const result: any[] = [];
-  userList.value.forEach((user) => {
-    user.projectUserRoles.forEach((role) => {
-      result.push({
-        nickName: user.nickName,
-        jobTitle: user.jobTitle,
-        diploma: user.diploma,
-        phonenumber: user.phonenumber,
-        deptName: user.deptName,
-        projectUserRole: role
-      });
-    });
-  });
-  return result;
-});
-
-projectMembers();
-
-// 监听项目ID的变化
-watch(
-  () => props.projectId,
-  (newProjectId, oldProjectId) => {
-    activeName.value = 'first';
-    projectDetail(); // 确保在属性变化时调用获取详情的方法
-  },
-  { immediate: true }
-);
-
-// 获取项目详情
-function projectDetail() {
+const getProjectDetail = async () => {
   loading.value = true;
   activeNames.value = 'first';
-  getProjectDetails(props.projectId)
-    .then((resp) => {
-      projectDetails.value = resp.data;
-      projectDescription.value = projectDetails.value.projectInfoVO.projectDescription;
-      projectLeaders.value = projectDetails.value.projectUserVoList.find((member) => member.projectUserRoles.includes('0'));
-    })
-    .catch((error) => {
-      console.error('获取用户数据时出错：', error);
-    })
-    .finally(() => {
-      loading.value = false;
-    });
-}
+  await getProjectDetails(props.projectId).then((resp) => {
+    projectDetails.projectFundsVO = resp.data.projectFundsVO;
+    projectDetails.projectInfoVO = resp.data.projectInfoVO;
+    projectDetails.projectPlanVOList = resp.data.projectPlanVOList;
+    projectDetails.projectTargetVOList = resp.data.projectTargetVOList;
+    projectDetails.projectUserVoList = resp.data.projectUserVoList;
+    projectDetails.projectAttachmentVOList = resp.data.projectAttachmentVOList;
+  }).catch((error) => {
+    console.error('获取用户数据时出错：', error);
+  }).finally(() => {
+    loading.value = false;
+  })
+};
 
 const updateVisible = (value: boolean) => {
   emits('update:visible', value);
 };
+
+watch(
+  () => props.projectId,
+  ( newValue, oldValue) => {
+    getProjectDetail();
+  },
+  { immediate: true }
+);
 
 watch(
   () => props.visible,
